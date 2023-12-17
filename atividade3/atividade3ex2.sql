ee311767f97f1720688fd74e0af3e79212ce01a3
Gabriel Loureiro <loureirogabrieloficial@gmail.com>
2023-12-17 20:07:47 -0300

    Resolução atividades Sql

---
+++ atividade3/atividade3ex2.sql
@@ -0,0 +1,34 @@
+CREATE TABLE departamento (
+    id_departamento INT PRIMARY KEY,
+    nome_departamento VARCHAR(100),
+    descricao_departamento TEXT
+);
+
+CREATE TABLE gerente (
+    id_gerente INT PRIMARY KEY,
+    nome_gerente VARCHAR(100),
+    salario DOUBLE,
+    id_departamento INT,
+    FOREIGN KEY (id_departamento) REFERENCES departamento(id_departamento)
+);
+
+
+INSERT INTO departamento (id_departamento, nome_departamento, descricao_departamento)
+VALUES (1, 'Vendas', 'Setor responsável por vendas de produtos');
+
+INSERT INTO gerente (id_gerente, nome_gerente, salario, id_departamento)
+VALUES (101, 'João Silva', 5000.00, 1);
+
+
+SELECT d.nome_departamento, g.nome_gerente, g.salario
+FROM departamento AS d
+JOIN gerente AS g ON d.id_departamento = g.id_departamento
+ORDER BY d.nome_departamento;
+
+
+
+SELECT d.nome_departamento, AVG(g.salario) AS media_salarial
+FROM departamento AS d
+JOIN gerente AS g ON d.id_departamento = g.id_departamento
+GROUP BY d.nome_departamento
+HAVING media_salarial > 4000;
